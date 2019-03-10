e77ea43f8e99a782ac6a2a359d8c95565127507e
Claudio Rafael <c.rafaelneto7@gmail.com>
2019-03-10 20:05:24 -0300

    Escrevi a classe que representa a volta

---
+++ Turn.py
@@ -0,0 +1,8 @@
+class Turn():
+    def __init__(self, time, numb_of_turn, speed):
+        self.time = time
+        self.numb_of_turn = numb_of_turn
+        self.speed = speed
+
+    def set_hours(self, hours): 
+        self.hours = hours
